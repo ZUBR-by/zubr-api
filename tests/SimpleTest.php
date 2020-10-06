d55f0d73dd9bb0e09ae4d5fcff4bfb5416eb7c91
Alherd <algerd.blr@gmail.com>
2020-10-06 21:45:59 +0300

    Fixed tests

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -5,11 +5,31 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SimpleTest extends WebTestCase
 {
-    public function testShowPost()
+    public function testCommission()
     {
         $client = static::createClient();
 
-        $client->request('GET', '/commission/1');
+        $client->request('GET', 'http://localhost:9010/commission/1');
+
+        $this->assertTrue(true);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testMember()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', 'http://localhost:9010/member/1');
+
+        $this->assertTrue(true);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testOrganization()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', 'http://localhost:9010/organization/1');
 
         $this->assertTrue(true);
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
