59d1764fd0d6eadc932bac3a44b2b41a2bcb3d1b
Alherd <algerd.blr@gmail.com>
2020-11-29 11:27:44 +0200

    Added statistic action

---
+++ src/Courts/GetStatisticsAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Courts;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class GetStatisticsAction extends AbstractController
+{
+    /**
+     * @var Connection
+     */
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function __invoke() : JsonResponse
+    {
+        $tmp    = $this->connection->fetchAllAssociative(
+            <<<'TAG'
+SELECT COUNT(1) as num, LEFT(court_id, 2) as region, YEAR(timestamp) as year
+  FROM decisions
+ WHERE YEAR(timestamp) IN (2019, 2020)
+GROUP BY LEFT(court_id, 2), YEAR(timestamp)
+ORDER BY year DESC, region
+TAG
+        );
+        $result = ['2019' => [], '2020' => []];
+        foreach ($tmp as $row) {
+            $result[$row['year']][] = (int) $row['num'];
+        }
+
+        return $this->json([
+            'data' => $result,
+        ]);
+    }
+}
