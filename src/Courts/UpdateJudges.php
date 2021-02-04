0e1232de2bfe1f76e64084173c2305405e006233
Alherd <algerd.blr@gmail.com>
2021-02-04 13:34:28 +0200

    Updating tags as well

--- src/Courts/UpdateJudges.php
+++ src/Courts/UpdateJudges.php
@@ -30,6 +30,7 @@ class UpdateJudges extends Command
     protected function execute(InputInterface $input, OutputInterface $output) : int
     {
         $this->connection->transactional(function () use ($output, $input) {
+            $limit = ($input->getOption('verbose') ? 1 : 10000);
             foreach (iterateCSV($this->projectDir . '/datasets/courts/judge.csv') as [$csvId, $fullName, , $phone]) {
                 $id = $this->connection->fetchOne('SELECT id FROM judge WHERE id = ?', [$csvId]);
                 if (! $id) {
@@ -43,8 +44,54 @@ class UpdateJudges extends Command
                     );
                 }
             }
+            $this->connection->executeQuery('DELETE FROM judge_tag');
+            $inserts = $this->prepareInsertsTag('judge_tag', $limit);
+            foreach ($inserts as $index => $insert) {
+                $this->connection->executeQuery($insert);
+            }
         });
 
         return 0;
+    }
+
+    private function prepareInsertsTag(string $dataset, int $limit) : array
+    {
+        $inserts    = [];
+        $rows       = [];
+        $rowCounter = 0;
+        $lineNumber = 0;
+        $fields     = implode(
+            ', ',
+            [
+                'judge_id',
+                'tag',
+            ]
+        );
+        $sql        = 'INSERT INTO ' . $dataset . ' (' . $fields . ') VALUES ' . PHP_EOL;
+        foreach (iterateCSV($this->projectDir . '/datasets/courts/judge_tag.csv') as $row) {
+            $lineNumber++;
+            $row  = array_map(
+                function ($s) {
+                    return $s === 'NULL' ? 'NULL' : '"' . str_replace('"', '\"', $s) . '"';
+                },
+                $row
+            );
+            $temp = '(' . implode(',', $row) . ')';
+            if ($temp === '("")') {
+                throw new \LogicException();
+            }
+            $rows[] = $temp;
+            if ($rowCounter === $limit) {
+                $inserts[]  = $sql . implode(',' . PHP_EOL, $rows);
+                $rows       = [];
+                $rowCounter = 0;
+            }
+            $rowCounter++;
+        }
+        if (count($rows) > 0) {
+            $inserts[] = $sql . implode(',' . PHP_EOL, $rows);
+        }
+
+        return $inserts;
     }
 }
