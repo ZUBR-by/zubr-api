22876584dd461e730066c2cd9ccdc58d187bfabf
Alherd <algerd.blr@gmail.com>
2021-03-22 22:37:00 +0200

    Теперь можно добавлять

--- src/Courts/Entity/Trial.php
+++ src/Courts/Entity/Trial.php
@@ -7,12 +7,21 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Table(name="trial", schema="courts")
  * @ORM\Entity
  * @ApiResource(
- *    collectionOperations={"get"},
+ *    normalizationContext={"groups"={"private"}},
+ *    collectionOperations={
+ *      "get",
+ *      "post"={
+ *          "method"="POST",
+ *          "path"="/trial",
+ *          "security"="is_granted('ROLE_USER')"
+ *      },
+ *    },
  *    itemOperations={"get"}
  * )
  * @ApiFilter(
@@ -26,33 +35,38 @@ class Trial
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Groups({"private"})
      */
     private int $id;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=false, options={"default" : ""})
+     * @Groups({"private"})
      */
     private string $person;
 
     /**
      * @ORM\Column(type="string", length=1000, nullable=false, options={"default" : ""})
+     * @Groups({"private"})
      */
     private string $comment;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Courts\Entity\Court")
      * @ORM\JoinColumn(name="court_id", referencedColumnName="id")
+     * @Groups({"private"})
      */
     private ?Court $court;
 
     /**
      * @ORM\Column(type="json", nullable=false, options={"default" : "[]"})
+     * @Groups({"private"})
      */
     private array $articles;
 
     /**
      * @var DateTime
-     *
+     * @Groups({"private"})
      * @ORM\Column(type="datetime")
      */
     private $timestamp;
@@ -67,6 +81,9 @@ class Trial
         return $this->timestamp->format(DATE_ATOM);
     }
 
+    /**
+     * @Groups({"private"})
+     */
     public function getTimestampFormatted() : ?string
     {
         return $this->timestamp->format('d.m.Y H:i');
@@ -90,5 +107,30 @@ class Trial
     public function getArticles() : array
     {
         return $this->articles;
+    }
+
+    public function setPerson(string $person) : void
+    {
+        $this->person = $person;
+    }
+
+    public function setCourt(?Court $court) : void
+    {
+        $this->court = $court;
+    }
+
+    public function setTimestamp(DateTime $timestamp) : void
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    public function setArticles(array $articles) : void
+    {
+        $this->articles = $articles;
+    }
+
+    public function setComment(string $comment) : void
+    {
+        $this->comment = $comment;
     }
 }
